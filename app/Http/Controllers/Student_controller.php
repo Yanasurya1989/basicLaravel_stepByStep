cb73fa91ed9b909ed31b6876bb23a6ad06a0b75d
YanaWebProgrammer <teukita15@gmail.com>
2023-10-15 16:01:56 +0700

    coolection

--- app/Http/Controllers/Student_controller.php
+++ app/Http/Controllers/Student_controller.php
@@ -61,7 +61,67 @@ class Student_controller extends Controller
 
             // delete
         // Student::find(2)->delete(); //find digunakan agar data tidak terhapus semua, tapi hanya yg id tertentu
+
+        $nilai = [9,9,8,7,3,5,6,8,7,4,6,2,1,7,8,9,5, 100];
+        // $countNilai = count($nilai);
+        // $totalNilai = array_sum($nilai);
+        // $nilaiRatarata = $totalNilai / $countNilai;
+        // dd($nilaiRatarata);
+
+        // collection
+        // $ngitungNilaiRatarataTeh = collect($nilai)->avg();
+        // dd($ngitungNilaiRatarataTeh);
+
+        // ngecek apakah dalam arraynya ada nilai tertentu
+        // $cek = collect($nilai)->contains(12);
+        // dd($cek);
+
+        // $cek = collect($nilai)->contains(function($value){
+        //     return $value < 6;
+        // });
+        // dd($cek);
+
+        // methode diff : untuk mencari item yang tidak ada 
+        $menuA = ['burger', 'siomay','pizza', 'spageti', 'makaroni', 'martabak', 'bakso'];
+        $menuB = ['pizza', 'fried chicken','martabak', 'sayur asam', 'pecel lele', 'bakso'];
+
+        // method collect : untuk memfilter
+        $kosongDiA = collect($menuA)->diff($menuB);
+        // dd($kosongDiA);
+
+        $kosongDiB = collect($menuB)->diff($menuA);
+        // dd($kosongDiB);
+
+        $difilter = collect($nilai)->filter(function($value){
+            return $value > 7;
+        })->all();
+        // dd($difilter);
+
+        // method pluck mengambil valuenya saja dalam array
+        $biodata = [
+            ['nama' => 'mimin', 'umur' => 23],
+            ['nama' => 'maman', 'umur' => 43],
+            ['nama' => 'mumun', 'umur' => 16],
+            ['nama' => 'momon', 'umur' => 10],
+        ];
+        $cetak = collect($biodata)->pluck('nama')->all();
+        // dd($cetak);
+
+        // php native looping
+        $nilaiKaliDua = [];
+        foreach($nilai as $value){
+            array_push($nilaiKaliDua, $value * 2);
+        }
         
+        // dd($nilaiKaliDua);
+
+        // eloquent looping (method map)
+        $naon = collect($nilai)->map(function ($value){
+
+        return $value * 2;
+        })->all();
+
+        dd($naon);
     }
 
     /**
